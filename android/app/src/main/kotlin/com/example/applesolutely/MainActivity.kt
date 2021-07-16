d6696c6088f3da0a13aa9e4befec3c38869b902c
Helena Juliá <helena.julias@compusoftgroup.com>
2021-07-16 14:52:59 +0200

    initial comit

---
+++ android/app/src/main/kotlin/com/example/applesolutely/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.applesolutely
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(child: 
+    Scaffold(
+      
+    ));
+  }
+}
