b90edc69e3187fc4a6ef415c6c1ae44118fa686e
Helena Juliá <helena.julias@compusoftgroup.com>
2021-07-21 12:47:48 +0200

    updated main

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -16,6 +16,10 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   List<dynamic> toRemove = List.empty(growable: true);
+    bool _pinned = true;
+  bool _snap = false;
+  bool _floating = false;
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,53 +27,146 @@ class _MainScreenState extends State<MainScreen> {
 
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Your dictionaries'),
-          actions: [
-            if (toRemove.isNotEmpty)
-              Center(
-                child: Text(
-                  '${toRemove.length}',
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.w500),
-                ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+               pinned: _pinned,
+            snap: _snap,
+            floating: _floating,
+            expandedHeight: 160.0,
+            flexibleSpace: const FlexibleSpaceBar(
+              title: Text('SliverAppBar'),
+              background: FlutterLogo(),
+            ),
+            ),
+            const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 20,
+              child: Center(
+                child: Text('Scroll to see the SliverAppBar in effect.'),
               ),
-            if (toRemove.isNotEmpty)
-              IconButton(
-                onPressed: () => _removeDictionaries(),
-                icon: const Icon(Icons.delete_forever_outlined),
-              )
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('$index', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          )
           ],
-        ),
-        body: Column(
-          children: [
-            const DictionaryFormWidget(),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 5,
-                  mainAxisSpacing: 5,
-                ),
-                itemCount: BoxService.dictionaries.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final Dictionary d = BoxService.dictionaries.getAt(index)!;
-                  return GestureDetector(
-                    onLongPress: () => _addRemoveDictionaryFromRemoveList(index),
-                    onTap: () {
-                      if (toRemove.isNotEmpty) {
-                        _addRemoveDictionaryFromRemoveList(index);
-                      }
+          ),
+        bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: OverflowBar(
+            alignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('pinned'),
+                  Switch(
+                    onChanged: (bool val) {
+                      setState(() {
+                        _pinned = val;
+                      });
                     },
-                    child: DictionaryTileWidget(d, toRemove.contains(d.key)),
-                  );
-                },
+                    value: _pinned,
+                  ),
+                ],
               ),
-            )
-          ],
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('snap'),
+                  Switch(
+                    onChanged: (bool val) {
+                      setState(() {
+                        _snap = val;
+                        // Snapping only applies when the app bar is floating.
+                        _floating = _floating || _snap;
+                      });
+                    },
+                    value: _snap,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text('floating'),
+                  Switch(
+                    onChanged: (bool val) {
+                      setState(() {
+                        _floating = val;
+                        _snap = _snap && _floating;
+                      });
+                    },
+                    value: _floating,
+                  ),
+                ],
+              ),
+            ],
+          ),
+      ),
         ),
       ),
+      // child: Scaffold(
+      //   appBar: AppBar(
+      //     title: const Text('Your dictionaries'),
+      //     actions: [
+      //       if (toRemove.isNotEmpty)
+      //         Center(
+      //           child: Text(
+      //             '${toRemove.length}',
+      //             style: const TextStyle(
+      //                 fontSize: 16, fontWeight: FontWeight.w500),
+      //           ),
+      //         ),
+      //       if (toRemove.isNotEmpty)
+      //         IconButton(
+      //           onPressed: () => _removeDictionaries(),
+      //           icon: const Icon(Icons.delete_forever_outlined),
+      //         )
+      //     ],
+      //   ),
+      //   body: Column(
+      //     children: [
+      //       const DictionaryFormWidget(),
+      //       Expanded(
+      //         child: GridView.builder(
+      //           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      //             crossAxisCount: 4,
+      //             childAspectRatio: 1,
+      //             crossAxisSpacing: 5,
+      //             mainAxisSpacing: 5,
+      //           ),
+      //           itemCount: BoxService.dictionaries.length,
+      //           itemBuilder: (BuildContext context, int index) {
+      //             final Dictionary d = BoxService.dictionaries.getAt(index)!;
+      //             return GestureDetector(
+      //               onLongPress: () => _addRemoveDictionaryFromRemoveList(index),
+      //               onTap: () {
+      //                 if (toRemove.isNotEmpty) {
+      //                   _addRemoveDictionaryFromRemoveList(index);
+      //                 }
+      //               },
+      //               child: DictionaryTileWidget(d, toRemove.contains(d.key)),
+      //             );
+      //           },
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
     );
   }
 
