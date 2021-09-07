f012e23e04523150868bddb5e77b7e51e0527d63
Hele <bloodthirsty.snow.white@gmail.com>
2021-09-07 20:53:12 +0200

    better form

--- lib/screens/dictionary_form_widget.dart
+++ lib/screens/dictionary_form_widget.dart
@@ -15,30 +15,37 @@ class DictionaryFormScreen extends StatefulWidget {
 
 class _DictionaryFormScreenState extends State<DictionaryFormScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _dictionaryController = TextEditingController();
-
+  final _dictionaryNameController = TextEditingController();
+  final _dictionarySummaryController = TextEditingController();
+  bool _isFavorite = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: Text('New Dictionary'), actions: [
+        appBar: AppBar(title: const Text('New Dictionary'), actions: [
           IconButton(
             onPressed: () {
               if (_formKey.currentState!.validate()) {
-                widget.callback(_dictionaryController.text.trim());
+                widget.callback(_dictionaryNameController.text.trim());
               }
             },
             icon: const Icon(Icons.save_outlined),
           ),
         ]),
-        body: Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: TextFormField(
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                      decoration: const BoxDecoration(color: Colors.red),
+                      height: 300,
+                      width: 300),
+                  //TODO: BUTTONS TO UPLOAD IMAGE
+                  const SizedBox(height: 10),
+                  TextFormField(
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter some text';
@@ -52,12 +59,37 @@ class _DictionaryFormScreenState extends State<DictionaryFormScreen> {
                       }
                       return null;
                     },
-                    controller: _dictionaryController,
-                    decoration:
-                        const InputDecoration(hintText: 'New dictionary name'),
+                    controller: _dictionaryNameController,
+                    decoration: const InputDecoration(
+                        hintText: 'It', labelText: 'Name'),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    controller: _dictionarySummaryController,
+                    decoration: const InputDecoration(
+                      hintText: 'Written by SKing',
+                      labelText: 'Summary',
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Mark as favorite',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      Switch(
+                        value: _isFavorite,
+                        onChanged: (bool value) {
+                          _isFavorite = value;
+                          setState(() {});
+                        },
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
