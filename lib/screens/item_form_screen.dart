1c9d811c897bf275c0cb32a67906fb2f14540332
Hele <bloodthirsty.snow.white@gmail.com>
2021-09-09 09:05:25 +0200

    added main image to item

--- lib/screens/item_form_screen.dart
+++ lib/screens/item_form_screen.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
 import 'package:applesolutely/models/item_model.dart';
-import 'package:applesolutely/services/converter_service.dart';
-import 'package:applesolutely/widgets/header_widget.dart';
+import 'package:applesolutely/widgets/pick_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_utils/src/extensions/internacionalization.dart';
-import 'package:images_picker/images_picker.dart';
 
 typedef ItemCallback = void Function(Item d);
 
@@ -46,48 +42,9 @@ class _ItemFormScreenState extends State<ItemFormScreen> {
           child: Form(
             key: _formKey,
             child: Column(children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.secondary),
-                height: MediaQuery.of(context).size.width / 4 * 3,
-                width: MediaQuery.of(context).size.width,
-                child: HeaderWidget(image: _base64Image),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TextButton(
-                    onPressed: _pickPicture,
-                    child: Row(
-                      children: [
-                        const Icon(Icons.add_photo_alternate_outlined),
-                        Text('pick_picture'.tr)
-                      ],
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: _takePicture,
-                    child: Row(
-                      children: [
-                        const Icon(Icons.add_a_photo_outlined),
-                        Text('take_picture'.tr)
-                      ],
-                    ),
-                  ),
-                  if (_base64Image.isNotEmpty)
-                    TextButton(
-                      onPressed: _deletePicture,
-                      child: Row(
-                        children: [
-                          const Icon(Icons.delete_outlined),
-                          Text('delete_picture'.tr)
-                        ],
-                      ),
-                    ),
-                ],
-              ),
-              const SizedBox(height: 10),
+              PickImageWidget((String? str) {
+                _updateImage(str, _base64Image);
+              })
             ]),
           ),
         ),
@@ -95,42 +52,8 @@ class _ItemFormScreenState extends State<ItemFormScreen> {
     );
   }
 
-  final double _quality = 0.4;
-  void _pickPicture() async {
-    List<Media>? res = await ImagesPicker.pick(
-      count: 1,
-      pickType: PickType.image,
-      quality: _quality,
-      cropOpt: CropOption(
-        aspectRatio: CropAspectRatio.wh4x3,
-        cropType: CropType.rect,
-      ),
-    );
-    _processImage(res);
-  }
-
-  void _takePicture() async {
-    List<Media>? res = await ImagesPicker.openCamera(
-      pickType: PickType.image,
-      quality: _quality,
-      cropOpt: CropOption(
-        aspectRatio: CropAspectRatio.wh4x3,
-        cropType: CropType.rect,
-      ),
-    );
-    _processImage(res);
-  }
-
-  void _processImage(List<Media>? res) async {
-    if (res != null) {
-      setState(() {
-        _base64Image = ConverterService.imageToBase64String(File(res[0].path));
-      });
-    }
-  }
-
-  void _deletePicture() {
-    _base64Image = '';
+  void _updateImage(String? str, String variable) {
+    variable = str ?? '';
     setState(() {});
   }
 }
